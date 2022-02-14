12a8b9483d58076b0c358a526b49c7b62c80f3ee
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-14 18:02:50 +0300

    update config

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ $database ="login";
 
 $conn = mysqli_connect($server, $username, $password, $database);
 if(!$conn){
-    die("<sript>alert('Connection Failed.')</sript>");
+    die("<script>alert('Connection Failed.')</script>");
 }
 ?>
