2a34c175ab9d839d20078c48a62afe3193c1895e
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-18 16:13:45 +0300

    new files

--- password.php
+++ password.php
@@ -11,11 +11,9 @@ if(isset($_POST['submit'])){
     $sql = "SELECT * FROM users WHERE email ='$email' AND password='$password'";
     $result = mysqli_query($conn, $sql);
     if($result-> num_rows >0){
-        $row = mysqli_fetch_assoc($result);
-        $_SESSION['username'] = $row['username'];
-        header("Location:welcome.php");
+        
     }else {
-        echo "<script>alert('Woops! Email or password is wrong.')</script>";
+       
     }
 }
 ?>
@@ -33,19 +31,19 @@ if(isset($_POST['submit'])){
 <body>
     <div class="container">
         <form action="" method="POST" class="login-email">
-            <p class="login-text" style="font-size: 2rem; font-weight:800;">Login</p>
+            <p class="login-text" style="font-size: 2rem; font-weight:800;">Reset Password</p>
             <div class="input-group">
-                <input type="email" placeholder="Email" name="email" value="<?php echo $_POST['email'];?>" required>
+                <input type="password" placeholder=" New Password" name="npassword"
+                    value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="input-group">
-                <input type="password" placeholder="Password" name="password" value="<?php echo $_POST['password'];?>"
-                    required>
+                <input type="password" placeholder="Confirm Password" name="cpassword"
+                    value="<?php echo $_POST['password']; ?>" required>
             </div>
             <div class="input-group">
-                <button name="submit" class="btn">Login</button>
+                <button name="submit" class="btn">Reset</button>
             </div>
-            <p class="login-register-text">Forgot Your password ? <a href="reset.php">Reset Here</a></p>
-            <p class="login-register-text">Don't have an Account ? <a href="register.php">Register Here</a></p>
+            <p class="login-register-text">Remebered your password ? <a href="index.php">login Here</a></p>
         </form>
     </div>
 </body>
